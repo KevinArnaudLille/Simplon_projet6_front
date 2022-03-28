2cbf70ceb3b0cabc3ac9775180080cb562ed0e88
abiba12 <ndeyediop.tech@gmail.com>
2022-03-28 11:06:34 +0200

    structure done

---
+++ src/app/components/liste-comptes/liste-comptes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-comptes',
+  templateUrl: './liste-comptes.component.html',
+  styleUrls: ['./liste-comptes.component.scss']
+})
+export class ListeComptesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
