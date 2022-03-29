cd9f317f3a0d860a6f9a738d5bca907fb1b048c7
abiba12 <ndeyediop.tech@gmail.com>
2022-03-29 10:29:37 +0200

    Ajout des fenetres

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './components/connexion/connexion.component';
 import { ListeClientsComponent } from './components/liste-clients/liste-clients.component';
+import { ListeComptesComponent } from './components/liste-comptes/liste-comptes.component';
 
 const routes: Routes = [
   { path: 'connexion', component: ConnexionComponent },
-  { path: 'listeClients', component: ListeClientsComponent }
+  { path: 'listeClients', component: ListeClientsComponent },
+  { path: 'listeComptes', component: ListeComptesComponent}
 ];
 
 @NgModule({
